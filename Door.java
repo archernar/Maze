2841c4ce5252df98d921326a39e3b5f77ea898b0
Michael Estes <michael@fastlight.com>
2021-03-27 13:49:48 +0000

    Initial Version

---
+++ Door.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+
+public class Door {
+    public Room room;
+    public Room roomTheDoorIsIn;
+    public boolean gonethroughalready;
+    public boolean locked;
+    public static int rand(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max + 1 - min) + min;
+    }
+   public Door(Room r) {
+       this.roomTheDoorIsIn = r;
+   }
+
+   public void attach(Room r) {
+       this.room = r;
+       this.gonethroughalready = false;
+       this.locked = false;
+   }
+
+}
